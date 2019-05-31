00f78cc797dd030b3a41924724ae22cfb49b8c60
Pano Skylakis <pano_skylakis@outlook.com>
2019-06-01 11:20:29 +1200

    add createBoard

--- createBoard.js
+++ createBoard.js
@@ -1,5 +1,14 @@
 function createBoard (size) {
-
+    let board = []
+    for (let i = 0; i < size; i++) {
+        board[i] = []
+        for (let j = 0; j < size; j++) {
+            board[i][j] = 0
+        }
+    }
+    return board
 }
+
+console.log(createBoard(3))
 
 module.exports = createBoard
