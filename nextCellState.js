e70d9f3a316e3ef28eb21e4beb3afcf1e91d7743
Pano Skylakis <pano_skylakis@outlook.com>
2019-06-02 19:06:51 +1200

    add nextCellState

--- nextCellState.js
+++ nextCellState.js
@@ -3,7 +3,16 @@ const isUnderPopulated = require('./isUnderPopulated')
 const isRessurectable = require('./isRessurectable')
 
 function nextCellState (cellState, neighbourCount) {
-
+    if (isOverPopulated(neighbourCount)) {
+        return cellState = false;
+    } else if (isUnderPopulated(neighbourCount)) {
+        return cellState = false;
+    } else if (isRessurectable(neighbourCount)) {
+        return cellState = true
+    } else {
+        return true;
+    }
 }
+
 
 module.exports = nextCellState
