b3a43728efbd3749a397c115541a7e3148c1da9f
Pano Skylakis <pano_skylakis@outlook.com>
2019-06-02 18:41:33 +1200

    add countAliveNeighbours

--- countAliveNeighbours.js
+++ countAliveNeighbours.js
@@ -1,7 +1,14 @@
 const getNeighbours = require('./getNeighbours')
 
 function countAliveNeighbours (cellRow, cellColumn, board) {
-
+    let count = 0;
+    let neighbourArr = getNeighbours(cellRow, cellColumn, board)
+    neighbourArr.forEach(element => {
+        if (element) {
+            count++
+        }
+    });
+    return count;
 }
 
 module.exports = countAliveNeighbours
